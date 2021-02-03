2b51d2ccada0bf906ac217d350f86040bed66888
Evelyn Cabrera <evelyn.2002.09@gmail.com>
2021-02-02 23:57:21 -0300

    tercera clase

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,71 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+
+	let n1;
+	let n2;
+    let resultado;
+	
+    n1 =parseInt(document.getElementById("txtIdNumeroUno").value);
+
+    n2 =parseInt(document.getElementById("txtIdNumeroDos").value);
+
+    resultado = n1 + n2;
+
+    alert (" la suma es " + resultado);
+
+
 }
 
 function restar()
 {
-	
+	let n1;
+	let n2;
+    let resultado;
+
+	n1 =parseInt(document.getElementById("txtIdNumeroUno").value);
+
+	n2 =parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	resultado = n1 - n2;
+
+	alert ("el resto es " + resultado);
+
+ 	
+
 }
 
 function multiplicar()
 { 
+    let n1;
+	let n2;
+    let resultado;
+
+	n1 =parseInt(document.getElementById("txtIdNumeroUno").value);
+
+    n2 =parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	resultado = n1 * n2;
+
+	alert ("el multiplicacion es " + resultado);
+
+
 	
 }
 
 function dividir()
 {
-	
+	let n1;
+	let n2;
+    let resultado;
+
+	n1 =parseInt(document.getElementById("txtIdNumeroUno").value);
+
+    n2 =parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	resultado = n1 / n2;
+
+	alert ("el cociente es " + resultado);
 }
 
+// hay que tener en cuenta de pedir un espacio de memoria si o si
--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -6,5 +6,23 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
-	alert("ok");
+	let sueldo;
+	let nuevoSueldo;
+	let aumento;
+	// para  calcular nuevo sueldo deberia sumarle al sueldo original el aumento 
+	// para obtener el porcentaje de un valor se hace lo siguiente; se multiplica el valor por el porcentaje y se divide por 100
+	
+	sueldo = parseFloat(document.getElementById("txtIdSueldo").value);
+
+	aumento = sueldo * 10 / 100;
+    
+	// para reducir el 10 / 100 se puede poner 0.10 ya que es una constante, aunque tambien se puede reducir mas 0.1 , ya que el 0 de la derecha es irrelevante
+	//y se puede reducir aun mas, .1, ya que el 0 de la izquierda tambien es irrelevante
+
+	nuevoSueldo = sueldo + aumento;
+	
+	document.getElementById("txtIdResultado").value = nuevoSueldo;
+	
+	
+	 
 }
